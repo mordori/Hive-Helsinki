f461194f558a712e5aeb479019b694e445ac73e8
Mika Yli-Pentti <mika.ylipentti@gmail.com>
2025-04-25 18:13:33 +0300

    libft update

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,13 +6,33 @@
 /*   By: myli-pen <myli-pen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 14:49:20 by myli-pen          #+#    #+#             */
-/*   Updated: 2025/04/25 14:50:27 by myli-pen         ###   ########.fr       */
+/*   Updated: 2025/04/25 17:53:37 by myli-pen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+
+static int	ft_isspace(char c)
+{
+	return (c == ' ' || (c >= '\t' && c <= '\r'));
+}
 
 int	ft_atoi(const char *nptr)
 {
+	int	sign;
+	int	number;
 
+	sign = 1;
+	number = 0;
+	while (ft_isspace(*nptr))
+		++nptr;
+	if (*nptr == '-')
+	{
+		sign = -1;
+		++nptr;
+	}
+	while (ft_isdigit(*nptr))
+		number = number * 10 + *nptr++ - '0';
+	return (sign * number);
 }
--- libft/tests/test_atoi.c
+++ libft/tests/test_atoi.c
@@ -12,17 +12,32 @@
 
 #include "libft_tests.h"
 
-void test_atoi(int c)
+void test_atoi(char *nptr)
 {
-	print_c(c);
-	printf("atoi: \t%d\n", atoi(c));
-	printf("ft_atoi \t%d\n", ft_atoi(c));
-	test_assert(atoi, ft_atoi, c);
+	printf("nptr: \t\t\"%s\"\n", nptr);
+	int orig = atoi(nptr);
+	int ft = ft_atoi(nptr);
+	printf("atoi: \t\t%d\n", orig);
+	printf("ft_atoi: \t%d\n", ft);
+	assert(orig == ft);
+	printf("assert "OK"\n\n");
 }
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
-	test_all(test_atoi);
-
+	test_atoi("123");
+	test_atoi("12s3");
+	test_atoi("\67octal");
+	test_atoi("\x32hex");
+	test_atoi("+ - -123    ");
+	test_atoi("+-123    ");
+	test_atoi("-123    ");
+	test_atoi("  -123    ");
+	test_atoi("  --123    ");
+	test_atoi("  \t  123");
+	test_atoi("2147483647");
+	test_atoi("-2147483648");
+	if(argc == 2)
+		test_atoi(argv[1]);
 	return (0);
 }
